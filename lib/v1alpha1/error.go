02f93d9b10b7592ffe570fe17fb8a4b900be3c7f
Mohamed Qadir <moqadir@cisco.com>
2019-09-06 16:09:16 +0530

    Added prometheus counters.

---
+++ lib/v1alpha1/error.go
@@ -0,0 +1,13 @@
+package v1alpha1
+
+// Counter errors.
+const (
+	ErrCounterCreateFailed  = Error("Failed to create counter.")
+	ErrCounterDestroyFailed = Error("Failed to remove counter.")
+)
+
+// Error represents an OF error.
+type Error string
+
+// Error returns the error as a string.
+func (e Error) Error() string { return string(e) }
