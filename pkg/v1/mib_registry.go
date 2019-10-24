1dece741a9bd9b43a8663d60eb1221cdd43d3024
Mohamed Qadir <moqadir@cisco.com>
2019-10-24 08:50:13 +0530

    Updated Mibs definition with comments and ShortName function.

--- pkg/v1/mib_registry.go
+++ pkg/v1/mib_registry.go
@@ -1,5 +1,6 @@
 package v1
 
+// Represents Mib description.
 type Mib struct {
 	Name        string
 	Description string
@@ -8,7 +9,18 @@ type Mib struct {
 }
 
 type MibRegistry interface {
-	Mib(string) Mib       // Return Mib for given OID.
-	String(string) string // Return display string for given OID.
-	Load(map[string]Mib)  // Load given map[oid]Mib into registry.
+	// Return Mib for given OID.
+	Mib(string) Mib
+
+	// Return Mib for given OID.
+	// Translate each node in OID to its corresponding name, if MIB has its definition, else use the number.
+	// Ex : 1.3.6.1.2.1.11.19 -> iso.org.dod.internet.mgmt.mib-2.snmp.snmpInTraps.
+	//      1.3.6.1.2.1.11.19.54334 -> iso.org.dod.internet.mgmt.mib-2.snmp.snmpInTraps.54334.
+	String(string) string
+
+	// Translate the last node to its name. Ex: 1.3.6.1.2.1.11.19 -> snmpInTraps.
+	ShortString(string) string
+
+	// Load given map[oid]Mib into registry.
+	Load(map[string]Mib)
 }
