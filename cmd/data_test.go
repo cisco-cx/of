232ebf6b5cf9fe140363d616720b58b6a1b72120
Mohamed Qadir <moqadir@cisco.com>
2019-11-01 23:14:13 +0530

    Added test cases for cmd/snmp.

---
+++ cmd/data_test.go
@@ -0,0 +1,360 @@
+package cmd_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	of "github.com/cisco-cx/of/pkg/v2"
+	mib_registry "github.com/cisco-cx/of/wrap/mib/v2"
+)
+
+var testEvents = `[{
+    "apiVersion": "v1alpha1",
+    "kind": "SNMPTrap",
+    "receipts": {
+      "snmptrapd": {
+        "source": {
+          "hostname": "localhost",
+          "transportLayerProtocol": "UDP",
+          "address": "::1",
+          "port": "48381",
+          "internetLayerProtocol": "IPv6"
+        },
+        "timestamp": "2019-04-26T03:46:57Z",
+        "pduSecurity": "TRAP2, SNMP v3, user user-sha-aes128, context",
+        "vars": [
+          {
+            "oid": ".1.3.6.1.2.1.1.3.0",
+            "type": "Timeticks",
+            "value": "(123) 0:00:01.23"
+          },
+          {
+            "oid": ".1.3.6.1.6.3.1.1.4.1.0",
+            "type": "OID",
+            "value": ".1.3.6.1.4.1.8164.2.44"
+          },
+          {
+            "oid": ".1.3.6.1.4.1.65000.1.1.1.1.1",
+            "type": "STRING",
+            "value": "foo"
+          },
+          {
+            "oid": ".1.3.6.1.4.1.65000.1.1.1.1.1",
+            "type": "STRING",
+            "value": "bar"
+          }
+        ]
+      },
+      "filebeat": {
+        "agent": {
+          "id": "c9da5463-1d21-405e-afe8-c3aa1b7d3bba",
+          "hostname": "vagrant",
+          "ephemeral_id": "543bb87e-17ca-4aa3-a9fb-cc67aa28c5c4",
+          "type": "filebeat",
+          "version": "7.0.0"
+        },
+        "ecs": {
+          "version": "1.0.0"
+        },
+        "log": {
+          "offset": 0,
+          "file": {
+            "path": ""
+          }
+        },
+        "@timestamp": "2019-04-26T03:46:57.941Z",
+        "@version": "1",
+        "message": "SNMPTRAP timestamp=[2019-04-26T03:46:57Z] hostname=[localhost] address=[UDP/IPv6: [::1]:48381] pdu_security=[TRAP2, SNMP v3, user user-sha-aes128, context ] vars[.1.3.6.1.2.1.1.3.0 = Timeticks: (123) 0:00:01.23\t.1.3.6.1.6.3.1.1.4.1.0 = OID: .1.3.6.1.6.3.1.1.5.1\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"foo\"\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"bar\"]",
+        "input": {
+          "type": "stdin"
+        },
+        "host": {
+          "name": "vagrant"
+        }
+      },
+      "logstash": {
+        "tags": [
+          "beats_input_codec_plain_applied"
+        ]
+      }
+    }
+  },{
+    "apiVersion": "v1alpha1",
+    "kind": "SNMPTrap",
+    "receipts": {
+      "snmptrapd": {
+        "source": {
+          "hostname": "localhost",
+          "transportLayerProtocol": "UDP",
+          "address": "::1",
+          "port": "48381",
+          "internetLayerProtocol": "IPv6"
+        },
+        "timestamp": "2019-04-26T03:46:57Z",
+        "pduSecurity": "TRAP2, SNMP v3, user user-sha-aes128, context",
+        "vars": [
+          {
+            "oid": ".1.3.6.1.2.1.1.3.0",
+            "type": "Timeticks",
+            "value": "(123) 0:00:01.23"
+          },
+          {
+            "oid": ".1.3.6.1.6.3.1.1.4.1.0",
+            "type": "OID",
+            "value": ".1.3.6.1.4.1.8164.2.43"
+          },
+          {
+            "oid": ".1.3.6.1.4.1.65000.1.1.1.1.1",
+            "type": "STRING",
+            "value": "foo"
+          },
+          {
+            "oid": ".1.3.6.1.4.1.65000.1.1.1.1.1",
+            "type": "STRING",
+            "value": "bar"
+          }
+        ]
+      },
+      "filebeat": {
+        "agent": {
+          "id": "c9da5463-1d21-405e-afe8-c3aa1b7d3bba",
+          "hostname": "vagrant",
+          "ephemeral_id": "543bb87e-17ca-4aa3-a9fb-cc67aa28c5c4",
+          "type": "filebeat",
+          "version": "7.0.0"
+        },
+        "ecs": {
+          "version": "1.0.0"
+        },
+        "log": {
+          "offset": 0,
+          "file": {
+            "path": ""
+          }
+        },
+        "@timestamp": "2019-04-26T03:46:57.941Z",
+        "@version": "1",
+        "message": "SNMPTRAP timestamp=[2019-04-26T03:46:57Z] hostname=[localhost] address=[UDP/IPv6: [::1]:48381] pdu_security=[TRAP2, SNMP v3, user user-sha-aes128, context ] vars[.1.3.6.1.2.1.1.3.0 = Timeticks: (123) 0:00:01.23\t.1.3.6.1.6.3.1.1.4.1.0 = OID: .1.3.6.1.6.3.1.1.5.1\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"foo\"\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"bar\"]",
+        "input": {
+          "type": "stdin"
+        },
+        "host": {
+          "name": "vagrant"
+        }
+      },
+      "logstash": {
+        "tags": [
+          "beats_input_codec_plain_applied"
+        ]
+      }
+    }
+  },{
+    "apiVersion": "v1alpha1",
+    "kind": "SNMPTrap",
+    "receipts": {
+      "snmptrapd": {
+        "source": {
+          "hostname": "localhost",
+          "transportLayerProtocol": "UDP",
+          "address": "::1",
+          "port": "48381",
+          "internetLayerProtocol": "IPv6"
+        },
+        "timestamp": "2019-04-26T03:46:57Z",
+        "pduSecurity": "TRAP2, SNMP v3, user user-sha-aes128, context",
+        "vars": [
+
+		{
+			"oid":   ".1.3.6.1.6.1.1.1.4.1",
+			"value": ".1.3.6.1.4.1.8164.1.2.1.1.1"
+		},
+		{
+			"oid":   ".1.3.6.1.4.1.8164.1.2.1.1.1",
+			"value": "14"
+		},
+		{
+			"oid":   ".1.3.6.1.4.1.24961.2.103.1.1.5.1.2",
+			"value": "package-load-failure"
+		},
+		{
+			"oid":   ".1.3.6.1.2.1.1.3.0",
+			"type":  "Timeticks",
+			"value": "(123) 0:00:01.23"
+		},
+		{
+			"oid":   ".1.3.6.1.6.3.1.1.4.1",
+			"type":  "OID",
+			"value": ".1.3.6.1.4.1.8164.2.13"
+		},
+		{
+			"oid":   ".1.3.6.1.6.3.1.1.4.1.0",
+			"type":  "OID",
+			"value": ".1.3.6.1.4.1.8164.2.44"
+		},
+		{
+			"oid":   ".1.3.6.1.4.1.8164.2.44",
+			"type":  "STRING",
+			"value": "foo"
+		},
+		{
+			"oid":   ".1.3.6.1.6.3.1.1.4.1.1",
+			"type":  "OID",
+			"value": ".1.3.6.1.4.1.8164.2.45"
+		},
+		{
+			"oid":   ".1.3.6.1.4.1.8164.2.45",
+			"type":  "OID",
+			"value": ".1.3.6.1.4.1.65000.1.1.1.1.1"
+		},
+		{
+			"oid":   ".1.3.6.1.4.1.65000.1.1.1.1.1",
+			"type":  "STRING",
+			"value": "bar"
+		}
+        ]
+      },
+      "filebeat": {
+        "agent": {
+          "id": "c9da5463-1d21-405e-afe8-c3aa1b7d3bba",
+          "hostname": "vagrant",
+          "ephemeral_id": "543bb87e-17ca-4aa3-a9fb-cc67aa28c5c4",
+          "type": "filebeat",
+          "version": "7.0.0"
+        },
+        "ecs": {
+          "version": "1.0.0"
+        },
+        "log": {
+          "offset": 0,
+          "file": {
+            "path": ""
+          }
+        },
+        "@timestamp": "2019-04-26T03:46:57.941Z",
+        "@version": "1",
+        "message": "SNMPTRAP timestamp=[2019-04-26T03:46:57Z] hostname=[localhost] address=[UDP/IPv6: [::1]:48381] pdu_security=[TRAP2, SNMP v3, user user-sha-aes128, context ] vars[.1.3.6.1.2.1.1.3.0 = Timeticks: (123) 0:00:01.23\t.1.3.6.1.6.3.1.1.4.1.0 = OID: .1.3.6.1.6.3.1.1.5.1\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"foo\"\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"bar\"]",
+        "input": {
+          "type": "stdin"
+        },
+        "host": {
+          "name": "vagrant"
+        }
+      },
+      "logstash": {
+        "tags": [
+          "beats_input_codec_plain_applied"
+        ]
+      }
+    }
+  }]`
+
+// Test mibs data.
+func mibRegistry(t *testing.T) *mib_registry.MIBRegistry {
+	mibs := map[string]of.MIB{
+		".1.3.6.1.2.1.1.3.0": of.MIB{
+			Name: "oid1",
+		},
+		".1.3.6.1.6.3.1.1.4.1.0": of.MIB{
+			Name: "oid2",
+		},
+		".1.3.6.1.4.1.8164.2.44": of.MIB{
+			Name: "oid3",
+		},
+		".1.3.6.1.4.1.8164.2.45": of.MIB{
+			Name: "oid4",
+		},
+		".1.3.6.1.4.1.65000.1.1.1.1.1": of.MIB{
+			Name: "oid5",
+		},
+	}
+
+	mr := mib_registry.New()
+	err := mr.Load(mibs)
+	require.NoError(t, err)
+	return mr
+}
+
+// Test trap vars data.
+func trapVars() *[]of.TrapVar {
+	return &[]of.TrapVar{
+		of.TrapVar{
+			Oid:   ".1.3.6.1.6.1.1.1.4.1",
+			Value: ".1.3.6.1.4.1.8164.1.2.1.1.1",
+		},
+		of.TrapVar{
+			Oid:   ".1.3.6.1.4.1.8164.1.2.1.1.1",
+			Value: "14",
+		},
+		of.TrapVar{
+			Oid:   ".1.3.6.1.4.1.24961.2.103.1.1.5.1.2",
+			Value: "package-load-failure",
+		},
+		of.TrapVar{
+			Oid:   ".1.3.6.1.2.1.1.3.0",
+			Type:  "Timeticks",
+			Value: "(123) 0:00:01.23",
+		},
+		of.TrapVar{
+			Oid:   ".1.3.6.1.6.3.1.1.4.1",
+			Type:  "OID",
+			Value: ".1.3.6.1.4.1.8164.2.13",
+		},
+		of.TrapVar{
+			Oid:   ".1.3.6.1.6.3.1.1.4.1.0",
+			Type:  "OID",
+			Value: ".1.3.6.1.4.1.8164.2.44",
+		},
+		of.TrapVar{
+			Oid:   ".1.3.6.1.4.1.8164.2.44",
+			Type:  "STRING",
+			Value: "foo",
+		},
+		of.TrapVar{
+			Oid:   ".1.3.6.1.6.3.1.1.4.1.1",
+			Type:  "OID",
+			Value: ".1.3.6.1.4.1.8164.2.45",
+		},
+		of.TrapVar{
+			Oid:   ".1.3.6.1.4.1.8164.2.45",
+			Type:  "OID",
+			Value: ".1.3.6.1.4.1.65000.1.1.1.1.1",
+		},
+		of.TrapVar{
+			Oid:   ".1.3.6.1.4.1.65000.1.1.1.1.1",
+			Type:  "STRING",
+			Value: "bar",
+		},
+	}
+}
+
+func TrapSource() *of.TrapSource {
+	return &of.TrapSource{
+		Address:  "192.168.1.28",
+		Hostname: "localhost",
+	}
+}
+
+func TrapReceipts() *of.Receipts {
+	return &of.Receipts{
+		Snmptrapd: of.Snmptrapd{
+			Timestamp: "2019-04-26T03:46:57Z",
+			Source:    *TrapSource(),
+			Vars:      *trapVars(),
+		},
+		Filebeat: of.Filebeat{
+			Message:   "SNMPTRAP timestamp=[2019-04-26T03:46:57Z] hostname=[localhost] address=[UDP/IPv6: [::1]:48381] pdu_security=[TRAP2, SNMP v3, user user-sha-aes128, context ] vars[.1.3.6.1.2.1.1.3.0 = Timeticks: (123) 0:00:01.23\t.1.3.6.1.6.3.1.1.4.1.0 = OID: .1.3.6.1.6.3.1.1.5.1\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"foo\"\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"bar\"]",
+			Timestamp: "2019-04-26T03:46:57.941Z",
+		},
+	}
+}
+
+func TrapEvents() []*of.PostableEvent {
+	events := []*of.PostableEvent{
+		&of.PostableEvent{
+			Document: of.Document{
+				Receipts: *TrapReceipts(),
+			},
+		},
+	}
+	return events
+}
---
+++ cmd/snmp_test.go
@@ -0,0 +1,212 @@
+package cmd_test
+
+import (
+	"bytes"
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/require"
+	snmp_cmd "github.com/cisco-cx/of/cmd"
+	of_v2 "github.com/cisco-cx/of/pkg/v2"
+	http "github.com/cisco-cx/of/wrap/http/v2"
+	logger "github.com/cisco-cx/of/wrap/logrus/v2"
+	snmp "github.com/cisco-cx/of/wrap/snmp/v2"
+)
+
+// Test SNMP mibs pre process
+func TestSNMPMIBsPreprocess(t *testing.T) {
+	cmd := cobra.Command{}
+
+	cmd.Flags().String("input-dir", "", "Path to MIBs directory.")
+	cmd.Flags().String("output-file", "none", "Path to MIBs cache file.")
+
+	viper.BindPFlags(cmd.Flags())
+
+	// Create temp. file
+	tmpFile, err := ioutil.TempFile(os.TempDir(), "cache-")
+	require.NoError(t, err)
+	fileName := tmpFile.Name()
+	tmpFile.Close()
+
+	defer os.Remove(fileName)
+
+	// Using mibs dir.
+	cmd.Flags().Parse([]string{
+		"--input-dir=test/snmp/mibs/",
+		"--output-file=" + fileName,
+	})
+
+	snmp_cmd.RunMibsPreProcess(&cmd, []string{})
+
+	// Compute check sum of generated file.
+	f, err := os.Open(fileName)
+	require.NoError(t, err)
+	defer f.Close()
+
+	h := md5.New()
+	_, err = io.Copy(h, f)
+	require.NoError(t, err)
+	computedHash := fmt.Sprintf("%x", h.Sum(nil))
+	require.Equal(t, "c81bbe474a939174697cc9eb784df704", computedHash)
+
+}
+
+// Test handler.run
+func TestSNMPHandler(t *testing.T) {
+
+	amAddress := "localhost:14932"
+
+	// Start fake AM Server.
+	srv := startFakeAM(t, amAddress)
+	defer srv.Shutdown()
+
+	cmd := cobra.Command{}
+
+	cmd.Flags().String("listen-address", "localhost:80", "host:port on which to listen, for SNMP trap events.")
+	cmd.Flags().String("am-address", "http://localhost:9093", "AlertManager's URL")
+	cmd.Flags().Duration("am-timeout", 1*time.Second, "Alertmanager timeout  (default: 10s)")
+	cmd.Flags().String("mibs-dir", "none", "Path to MIBs directory.")
+	cmd.Flags().String("cache-file", "none", "Path to MIBs cache file.")
+	cmd.Flags().String("config-dir", "", "Path to directory containing configs.")
+	cmd.Flags().Bool("throttle", true, "Trottle posts to Alertmanager (default: true)")
+	cmd.Flags().Int("post-time", 300, "Approx time in ms, that it takes to HTTP POST to AM. (default: 300)")
+	cmd.Flags().Int("sleep-time", 100, "Time in ms, to sleep between HTTP POST to AM. (default: 100)")
+	cmd.Flags().Int("send-time", 60000, "Time in ms, to complete HTTP POST to AM. (default: 60000)")
+
+	viper.BindPFlags(cmd.Flags())
+
+	// Using mibs dir.
+	cmd.Flags().Parse([]string{
+		"--listen-address=blah",
+
+		"--listen-address=localhost:14444",
+		"--am-address=http://" + amAddress,
+		"--mibs-dir=test/snmp/mibs/",
+		//"--am-timeout=1s",
+		//"--cache-file=/tmp/mibs.json",
+		"--config-dir=test/snmp/configs/",
+	})
+	checkHandler(t, cmd, amAddress)
+
+	// Using cache file that was just generated dir.
+	cmd.Flags().Parse([]string{
+		"--listen-address=blah",
+
+		"--listen-address=localhost:14444",
+		"--am-address=http://" + amAddress,
+		"--am-timeout=10s",
+		"--cache-file=test/snmp/cache_mibs.json",
+		"--config-dir=test/snmp/configs/",
+	})
+	checkHandler(t, cmd, amAddress)
+}
+
+func checkHandler(t *testing.T, cmd cobra.Command, amAddress string) {
+
+	config := snmp_cmd.SNMPConfig(&cmd)
+
+	log := logger.New()
+	service, err := snmp.NewService(log, config)
+	require.NoError(t, err)
+
+	handler := &snmp.Handler{
+		Config: config,
+		SNMP:   service,
+		Log:    log,
+	}
+
+	go handler.Run()
+
+	// Wait for server to start.
+	time.Sleep(3 * time.Second)
+	runHandlerChecks(t, config.ListenAddress)
+	handler.Shutdown()
+}
+
+func startFakeAM(t *testing.T, amAddress string) *http.Server {
+
+	hc := &of_v2.HTTPConfig{ListenAddress: amAddress, ReadTimeout: 1 * time.Second, WriteTimeout: 1 * time.Second}
+
+	srv := http.NewServer(hc)
+	srv.HandleFunc("/-/healthy", func(w of_v2.ResponseWriter, r of_v2.Request) {
+		w.WriteHeader(http.StatusBadGateway)
+	})
+	srv.HandleFunc("/api/v1/alerts", func(w of_v2.ResponseWriter, r of_v2.Request) {
+
+		var alerts []of_v2.Alert
+		err := json.NewDecoder(r.Body).Decode(&alerts)
+		require.NoError(t, err)
+		require.Len(t, alerts, 2)
+		expectedOIDStrs := []string{
+			"iso.org.dod.internet.private.enterprises.starentMIB.starentTraps.starAAAAccServerMisconfigured",
+			"iso.org.dod.internet.private.enterprises.starentMIB.starentTraps.starAAAAccServerMisconfigured",
+		}
+		eventOIDStrs := make([]string, 0)
+		for _, alert := range alerts {
+			for k, v := range alert.Annotations {
+				if k == "event_oid_string" {
+					eventOIDStrs = append(eventOIDStrs, v)
+				}
+			}
+		}
+		require.ElementsMatch(t, expectedOIDStrs, eventOIDStrs)
+	})
+
+	go func() {
+		err := srv.ListenAndServe()
+		require.NoError(t, err)
+	}()
+	return srv
+}
+
+func runHandlerChecks(t *testing.T, listenAddress string) {
+
+	// Test metrics
+	message := getResponse(t, 200, "http://"+listenAddress+"/metrics")
+	require.Contains(t, message, "promhttp_metric_handler_requests_total")
+
+	// Test Status
+	checkResponse(t, 200, "http://"+listenAddress+"/api/v2/status", "{ AlertManager Client for SNMP Traps {https://github.com/cisco-cx/am-client-snmp} success}")
+
+	// Test Posting SNMP event
+	dataBytes, err := json.Marshal(TrapEvents())
+	require.NoError(t, err)
+
+	data := bytes.NewBuffer(dataBytes)
+	c := http.NewClient()
+	req, err := http.NewRequest("Post", "http://"+listenAddress+"/api/v2/events", data)
+	require.NoError(t, err)
+	req.Header.Set("User-Agent", "test")
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := c.Do(req)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, res.StatusCode)
+}
+
+// HTTP client to hit server and check response.
+func checkResponse(t *testing.T, expectedStatusCode int, u, msg string) {
+	all := getResponse(t, expectedStatusCode, u)
+	require.Equal(t, msg, all)
+}
+
+// HTTP client to hit server and check response.
+func getResponse(t *testing.T, expectedStatusCode int, u string) string {
+
+	c := http.NewClient()
+	res, err := c.Get(u)
+	require.NoError(t, err)
+	require.Equal(t, expectedStatusCode, res.StatusCode)
+
+	all, err := ioutil.ReadAll(res.Body)
+	require.NoError(t, err)
+	return string(all)
+}
