3c074b45b466c3d8139d35af40d6ad12539c6453
Mohamed Qadir <moqadir@cisco.com>
2020-05-05 00:20:44 +0530

    Added test case to handle flap alerts.

--- wrap/snmp/v2/service_test.go
+++ wrap/snmp/v2/service_test.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 	of "github.com/cisco-cx/of/pkg/v2"
@@ -26,119 +25,67 @@ type testAlertService struct {
 // Test Notify function.
 func (as *testAlertService) Notify(alerts *[]of.Alert) error {
 	if len(*alerts) == 3 {
-		as.verifyClearNotify(alerts)
-	} else {
 		as.verifyFireNotify(alerts)
+	} else {
+		as.verifyClearNotify(alerts)
 	}
 	return nil
 }
 
 func (as *testAlertService) verifyFireNotify(alerts *[]of.Alert) {
-	expectedAlerts := []of.Alert{
-		of.Alert{
-			Labels: map[string]string{
-				"alert_name":        "starCard",
-				"alert_severity":    "error",
-				"source_address":    "192.168.1.28",
-				"source_hostname":   "localhost",
-				"star_slot_num":     "14",
-				"subsystem":         "epc",
-				"vendor":            "cisco",
-				"alert_fingerprint": "5dd1df6eff3119f4",
-				"alert_oid":         ".1.3.6.1.4.1.8164.1.2.1.1.1",
-			},
-			Annotations: map[string]string{
-				"event_id":                  "9dcc77fc-dda5-4edf-a683-64f2589036d6",
-				"event_oid":                 ".1.3.6.1.4.1.8164.1.2.1.1.1",
-				"event_type":                "error",
-				"source_address":            "192.168.1.28",
-				"source_hostname":           "localhost",
-				"event_description":         "",
-				"event_filebeat_timestamp":  "2019-04-26T03:46:57.941Z",
-				"event_name":                "unknown",
-				"event_oid_string":          "",
-				"event_rawtext":             "SNMPTRAP timestamp=[2019-04-26T03:46:57Z] hostname=[localhost] address=[UDP/IPv6: [::1]:48381] pdu_security=[TRAP2, SNMP v3, user user-sha-aes128, context ] vars[.1.3.6.1.2.1.1.3.0 = Timeticks: (123) 0:00:01.23\t.1.3.6.1.6.3.1.1.4.1.0 = OID: .1.3.6.1.6.3.1.1.5.1\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"foo\"\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"bar\"]",
-				"event_snmptrapd_timestamp": "2019-04-26T03:46:57Z",
-				"event_vars_json":           "[{\"oid\":\".1.3.6.1.6.1.1.1.4.1\",\"oid_string\":\"1.3.6.1.6.1.1.1.4.1\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.6.1.1.1.4.1\",\"type\":\"\",\"value\":\".1.3.6.1.4.1.8164.1.2.1.1.1\"},{\"oid\":\".1.3.6.1.4.1.8164.1.2.1.1.1\",\"oid_string\":\"1.3.6.1.4.1.8164.1.2.1.1.1\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.4.1.8164.1.2.1.1.1\",\"type\":\"\",\"value\":\"14\"},{\"oid\":\".1.3.6.1.4.1.24961.2.103.1.1.5.1.2\",\"oid_string\":\"1.3.6.1.4.1.24961.2.103.1.1.5.1.2\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.4.1.24961.2.103.1.1.5.1.2\",\"type\":\"\",\"value\":\"package-load-failure\"},{\"oid\":\".1.3.6.1.2.1.1.3.0\",\"oid_string\":\"1.3.6.1.2.1.1.3.0\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.2.1.1.3.0\",\"type\":\"Timeticks\",\"value\":\"(123) 0:00:01.23\"},{\"oid\":\".1.3.6.1.6.3.1.1.4.1\",\"oid_string\":\"1.3.6.1.6.3.1.1.4.1\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.6.3.1.1.4.1\",\"type\":\"OID\",\"value\":\".1.3.6.1.4.1.8164.2.13\"},{\"oid\":\".1.3.6.1.6.3.1.1.4.1.0\",\"oid_string\":\"1.3.6.1.6.3.1.1.4.1.0\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.6.3.1.1.4.1.0\",\"type\":\"OID\",\"value\":\".1.3.6.1.4.1.8164.2.44\"},{\"oid\":\".1.3.6.1.4.1.8164.2.44\",\"oid_string\":\"1.3.6.1.4.1.8164.2.44\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.4.1.8164.2.44\",\"type\":\"STRING\",\"value\":\"foo\"},{\"oid\":\".1.3.6.1.6.3.1.1.4.1.1\",\"oid_string\":\"1.3.6.1.6.3.1.1.4.1.1\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.6.3.1.1.4.1.1\",\"type\":\"OID\",\"value\":\".1.3.6.1.4.1.8164.2.45\"},{\"oid\":\".1.3.6.1.4.1.8164.2.45\",\"oid_string\":\"1.3.6.1.4.1.8164.2.45\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.4.1.8164.2.45\",\"type\":\"OID\",\"value\":\".1.3.6.1.4.1.65000.1.1.1.1.1\"},{\"oid\":\".1.3.6.1.4.1.65000.1.1.1.1.1\",\"oid_string\":\"1.3.6.1.4.1.65000.1.1.1.1.1\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.4.1.65000.1.1.1.1.1\",\"type\":\"STRING\",\"value\":\"bar\"}]",
-			},
-			GeneratorURL: "http://www.oid-info.com/get/1.3.6.1.4.1.8164.2.44",
+
+	expectedLabels := []map[string]string{
+		map[string]string{
+			"alert_fingerprint": "26c5384f07068e37",
+			"alert_oid":         ".1.3.6.1.4.1.8164.2.150",
+			"alert_severity":    "major",
+			"alertname":         "starTaskFailure",
+			"source_address":    "dead::beef",
+			"source_hostname":   "test-device-01",
+			"subsystem":         "config1",
+			"vendor":            "cisco",
+		},
+		map[string]string{
+			"alert_fingerprint": "60dfb17ae277c3c8",
+			"alert_oid":         ".1.3.6.1.4.1.8164.2.150",
+			"alert_severity":    "major",
+			"alertname":         "starTaskRestart",
+			"source_address":    "dead::beef",
+			"source_hostname":   "test-device-01",
+			"subsystem":         "config1",
+			"vendor":            "cisco",
+		},
+		map[string]string{
+			"alert_fingerprint": "a8e70c001d09366f",
+			"alert_oid":         ".1.3.6.1.4.1.8164.2.151",
+			"alert_severity":    "major",
+			"alertname":         "starTaskRestart",
+			"source_address":    "dead::beef",
+			"source_hostname":   "test-device-01",
+			"subsystem":         "config1",
+			"vendor":            "cisco",
 		},
 	}
 
-	var err error
-	expectedAlerts[0].StartsAt, err = time.Parse(time.RFC3339, "2019-04-26T03:46:57Z")
-	require.NoError(as.t, err)
-	require.ElementsMatch(as.t, expectedAlerts, *alerts)
+	compareLabels(as.t, alerts, expectedLabels)
 }
 
 func (as *testAlertService) verifyClearNotify(alerts *[]of.Alert) {
 
-	nonEmpty := 0
-	for idx, alert := range *alerts {
-		if alert.EndsAt.IsZero() == false {
-			nonEmpty += 1
-			(*alerts)[idx].EndsAt = time.Time{}
-		}
-	}
-
-	require.Equal(as.t, nonEmpty, 3)
-
-	expectedClearAlertTemplate := of.Alert{
-		Labels: map[string]string{
-			"alert_severity":    "error",
-			"alertname":         "nsoPackageLoadFailure",
-			"source_address":    "nso1.example.org",
-			"source_hostname":   "nso1.example.org",
-			"subsystem":         "nso",
-			"alert_fingerprint": "ab89464e06ae596f",
+	expectedLabels := []map[string]string{
+		map[string]string{
+			"alert_fingerprint": "26c5384f07068e37",
+			"alert_oid":         ".1.3.6.1.4.1.8164.2.150",
+			"alert_severity":    "major",
+			"alertname":         "starTaskFailure",
+			"source_address":    "dead::beef",
+			"source_hostname":   "test-device-01",
+			"subsystem":         "config1",
 			"vendor":            "cisco",
 		},
-		Annotations: map[string]string{
-			"event_type":                "clear",
-			"event_id":                  "9dcc77fc-dda5-4edf-a683-64f2589036d6",
-			"source_address":            "nso1.example.org",
-			"source_hostname":           "nso1.example.org",
-			"event_description":         "",
-			"event_filebeat_timestamp":  "2019-04-26T03:46:57.941Z",
-			"event_name":                "unknown",
-			"event_oid_string":          "",
-			"event_rawtext":             "SNMPTRAP timestamp=[2019-04-26T03:46:57Z] hostname=[localhost] address=[UDP/IPv6: [::1]:48381] pdu_security=[TRAP2, SNMP v3, user user-sha-aes128, context ] vars[.1.3.6.1.2.1.1.3.0 = Timeticks: (123) 0:00:01.23\t.1.3.6.1.6.3.1.1.4.1.0 = OID: .1.3.6.1.6.3.1.1.5.1\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"foo\"\t.1.3.6.1.4.1.65000.1.1.1.1.1 = STRING: \"bar\"]",
-			"event_snmptrapd_timestamp": "2019-04-26T03:46:57Z",
-			"event_vars_json":           "[{\"oid\":\".1.3.6.1.6.1.1.1.4.1\",\"oid_string\":\"1.3.6.1.6.1.1.1.4.1\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.6.1.1.1.4.1\",\"type\":\"\",\"value\":\".1.3.6.1.4.1.8164.1.2.1.1.1\"},{\"oid\":\".1.3.6.1.4.1.8164.1.2.1.1.1\",\"oid_string\":\"1.3.6.1.4.1.8164.1.2.1.1.1\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.4.1.8164.1.2.1.1.1\",\"type\":\"\",\"value\":\"14\"},{\"oid\":\".1.3.6.1.4.1.24961.2.103.1.1.5.1.2\",\"oid_string\":\"1.3.6.1.4.1.24961.2.103.1.1.5.1.2\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.4.1.24961.2.103.1.1.5.1.2\",\"type\":\"\",\"value\":\"package-load-failure\"},{\"oid\":\".1.3.6.1.2.1.1.3.0\",\"oid_string\":\"1.3.6.1.2.1.1.3.0\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.2.1.1.3.0\",\"type\":\"Timeticks\",\"value\":\"(123) 0:00:01.23\"},{\"oid\":\".1.3.6.1.6.3.1.1.4.1\",\"oid_string\":\"1.3.6.1.6.3.1.1.4.1\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.6.3.1.1.4.1\",\"type\":\"OID\",\"value\":\".1.3.6.1.4.1.8164.2.13\"},{\"oid\":\".1.3.6.1.6.3.1.1.4.1.0\",\"oid_string\":\"1.3.6.1.6.3.1.1.4.1.0\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.6.3.1.1.4.1.0\",\"type\":\"OID\",\"value\":\".1.3.6.1.4.1.8164.2.44\"},{\"oid\":\".1.3.6.1.4.1.8164.2.44\",\"oid_string\":\"1.3.6.1.4.1.8164.2.44\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.4.1.8164.2.44\",\"type\":\"STRING\",\"value\":\"foo\"},{\"oid\":\".1.3.6.1.6.3.1.1.4.1.1\",\"oid_string\":\"1.3.6.1.6.3.1.1.4.1.1\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.6.3.1.1.4.1.1\",\"type\":\"OID\",\"value\":\".1.3.6.1.4.1.8164.2.45\"},{\"oid\":\".1.3.6.1.4.1.8164.2.45\",\"oid_string\":\"1.3.6.1.4.1.8164.2.45\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.4.1.8164.2.45\",\"type\":\"OID\",\"value\":\".1.3.6.1.4.1.65000.1.1.1.1.1\"},{\"oid\":\".1.3.6.1.4.1.65000.1.1.1.1.1\",\"oid_string\":\"1.3.6.1.4.1.65000.1.1.1.1.1\",\"oid_uri\":\"http://www.oid-info.com/get/1.3.6.1.4.1.65000.1.1.1.1.1\",\"type\":\"STRING\",\"value\":\"bar\"}]",
-			"event_oid":                 ".1.3.6.1.4.1.8164.2.44",
-		},
-		EndsAt:       time.Time{},
-		GeneratorURL: "http://www.oid-info.com/get/1.3.6.1.4.1.8164.2.44",
 	}
 
-	OIDs := []map[string]string{
-		map[string]string{
-			"oid": ".1.3.6.1.4.1.24961.2.103.2.0.3",
-			"fp":  "ef451c2db05fdd5d",
-		},
-		map[string]string{
-			"oid": ".1.3.6.1.4.1.24961.2.103.2.0.4",
-			"fp":  "14b93f772f8125e0",
-		},
-		map[string]string{
-			"oid": ".1.3.6.1.4.1.24961.2.103.2.0.5",
-			"fp":  "ab89464e06ae596f",
-		},
-	}
-
-	expectedAlerts := make([]of.Alert, 0)
-	for _, val := range OIDs {
-		newAlert := of.Alert{}
-		alertJSON, _ := json.Marshal(expectedClearAlertTemplate)
-		err := json.Unmarshal(alertJSON, &newAlert)
-		require.NoError(as.t, err)
-		newAlert.Labels["alert_oid"] = val["oid"]
-		newAlert.Labels["alert_fingerprint"] = val["fp"]
-		require.NoError(as.t, err)
-		expectedAlerts = append(expectedAlerts, newAlert)
-	}
-
-	require.ElementsMatch(as.t, expectedAlerts, *alerts)
+	compareLabels(as.t, alerts, expectedLabels)
 }
 
 func TestSNMPService(t *testing.T) {
@@ -156,11 +103,23 @@ func TestSNMPService(t *testing.T) {
 	err := srv.ListenAndServe()
 	require.NoError(t, err)
 
-	// Send SNMP traps to server.
-	dataBytes, err := json.Marshal(TrapEvents())
+	docs := []of.Document{}
+	err = json.Unmarshal([]byte(StarEvents), &docs)
 	require.NoError(t, err)
 
-	data := bytes.NewBuffer(dataBytes)
+	events := make([]*of.PostableEvent, len(docs))
+	for i, doc := range docs {
+		events[i] = &of.PostableEvent{
+			Document: doc,
+		}
+	}
+
+	dataJson, err := json.Marshal(events)
+	require.NoError(t, err)
+
+	data := bytes.NewBuffer(dataJson)
+
+	// Send SNMP traps to server.
 	c := http.NewClient()
 	req, err := http.NewRequest("Post", "http://"+addr, data)
 	require.NoError(t, err)
@@ -183,7 +142,7 @@ func initService(t *testing.T, namespace string) *snmp.Service {
 	//l.SetLevel("debug")
 
 	// Decode configs files.
-	r := strings.NewReader(YamlContent)
+	r := strings.NewReader(YamlConfigs)
 	configs := yaml.Configs{}
 	err := configs.Decode(r)
 	require.NoError(t, err)
@@ -191,7 +150,7 @@ func initService(t *testing.T, namespace string) *snmp.Service {
 	v2Config := of_snmp.V2Config(configs)
 
 	// Prepare MIBS registry
-	mr := mibRegistry(t)
+	mr := newFakeMibRegistry()
 
 	// Prepare lookup.
 	lookup := snmp.Lookup{Configs: v2Config, MR: mr, Log: l}
@@ -221,4 +180,12 @@ func initService(t *testing.T, namespace string) *snmp.Service {
 		SNMPConfig: cfg,
 	}
 	return s
+}
+
+func compareLabels(t *testing.T, alerts *[]of.Alert, expectedLabels []map[string]string) {
+	alertLabels := make([]map[string]string, len(*alerts))
+	for i, a := range *alerts {
+		alertLabels[i] = a.Labels
+	}
+	require.Equal(t, expectedLabels, alertLabels)
 }
